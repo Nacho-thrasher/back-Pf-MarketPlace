7ae7baadaa927d86054f672f45d75e9ae4fb028d
Nacho-Thrasher <nachoburgos1995@gmail.com>
2022-06-06 16:22:00 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const books=require('./books')
+const categories=require('./categories')
+const userRouter = require('./user.routes');
+const roleRouter = require('./role.routes');
+const authRouter = require('./auth.routes');
+const paypal = require('./paypal')
+const reviews= require('./reviews')
+const favourites= require('./favourites')
+const email = require('./email.routes')
+const support = require('./support.routes')
+
+const router = Router();
+
+router.use('/books', books)
+router.use('/categories', categories)
+
+// Configurar los routers
+router.use('/email', email)
+router.use('/user', userRouter);
+router.use('/role', roleRouter);
+router.use('/auth', authRouter);
+router.use('/paypal', paypal)
+router.use('/favourites', favourites)
+router.use('/reviews', reviews)
+router.use('/support', support)
+
+module.exports = router;
